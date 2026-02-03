ac8e04af963cbc2e14775a057a6ee6d4ff121b58
lumi <136560713+Lumi-snow@users.noreply.github.com>
2026-02-03 16:09:10 +0900

    ランタンの明るさに段階付けした

---
+++ Assets/Scripts/LanternLight/LanarernStrengthController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanarernStrengthController : MonoBehaviour
+{
+    private int _strengthLevel;
+
+    [Header("ランタンの Light")]
+    [SerializeField] private Light _lanternLight;
+    [Header("ランタンの強さ上限")]
+    [SerializeField] private int _strengthNumMax;
+    [Header("ランタンの各段階の設定")]
+    [SerializeField] private LanternStrengthData[] _strengthLevels;
+
+    [System.Serializable]
+    public class LanternStrengthData
+    {
+        public float intensity;
+        public float indirectMultiplier;
+        public float range;
+    }
+
+    void Start()
+    {
+        if (_lanternLight == null)
+        {
+            Debug.LogError("Light が設定されていません");
+            return;
+        }
+        _strengthLevel = _strengthNumMax;
+        ApplyStrength();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O) && _strengthLevel < _strengthNumMax)
+        {
+            _strengthLevel++;
+            ApplyStrength();
+        }
+        if (Input.GetKeyDown(KeyCode.L) && _strengthLevel > 0)
+        {
+            _strengthLevel--;
+            ApplyStrength();
+        }
+    }
+
+    //各段階のlight設定を反映させる
+    void ApplyStrength()
+    {
+        LanternStrengthData data = _strengthLevels[_strengthLevel];
+
+        _lanternLight.intensity = data.intensity;
+        _lanternLight.range = data.range;
+        _lanternLight.bounceIntensity = data.indirectMultiplier;
+    }
+}
